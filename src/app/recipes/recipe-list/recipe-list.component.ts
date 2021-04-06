64a116d0fafab762e526bf90de9da7a2c23aa777
Viktor <malyshkin.viktor@icloud.com>
2021-04-06 16:52:59 +0300

    s3 p53 - Adding Content to the Recipes Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { RecipeModel } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = []
+  recipes: RecipeModel[] = [
+    new RecipeModel('A Test Recipe', 'This is simply a test', 'https://static.onecms.io/wp-content/uploads/sites/9/2020/03/19/birria-tacos-FT-RECIPE0420-1.jpg')
+  ]
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -9,3 +9,4 @@ export class RecipeModel {
     this.imagePath = imagePath
   }
 }
+
