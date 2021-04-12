a31a436fff5846c134bab89c20f31926e1ae5546
Viktor <malyshkin.viktor@icloud.com>
2021-04-12 16:02:06 +0300

    Rename project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { browser, logging } from 'protractor';
 import { AppPage } from './app.po';
 
-describe('workspace-project App', () => {
+describe('workspace-udemy-prj-basics App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('project app is running!');
+    expect(await page.getTitleText()).toEqual('udemy-prj-basics app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'project'
+  title = 'udemy-prj-basics'
   loadedFeature = 'recipe'
 
   onNavigate(feature: string): void {
