df545b78d354c3af722218c79acb8c89eddebf35
Viktor <malyshkin.viktor@icloud.com>
2021-04-13 20:34:37 +0300

    s10 p120. Adding the Shopping List Service

--- src/app/services/shopping-list.service.ts
+++ src/app/services/shopping-list.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {IngredientModel} from "../shared/ingredient.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
+  private ingredients: IngredientModel[] = [
+    new IngredientModel('Apples', 5),
+    new IngredientModel('Tomatoes', 10),
+  ]
+
+
+  getIngredients(): IngredientModel[] {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: IngredientModel) {
+    this.ingredients.push(ingredient)
+  }
 
   constructor() { }
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core'
 import { IngredientModel } from '../../shared/ingredient.model'
+import {ShoppingListService} from "../../services/shopping-list.service";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,9 +10,8 @@ import { IngredientModel } from '../../shared/ingredient.model'
 export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput') nameInputRef: ElementRef
   @ViewChild('amountInput') amountInputRef: ElementRef
-  @Output() ingredientAdded = new EventEmitter<IngredientModel>()
 
-  constructor() {
+  constructor(private shoppingListService: ShoppingListService) {
   }
 
   ngOnInit(): void {
@@ -21,6 +21,6 @@ export class ShoppingEditComponent implements OnInit {
     const ingName = this.nameInputRef.nativeElement.value
     const ingAmount = this.amountInputRef.nativeElement.value
     const newIngredient = new IngredientModel(ingName, ingAmount)
-    this.ingredientAdded.emit(newIngredient)
+    this.shoppingListService.addIngredient(newIngredient)
   }
 }
