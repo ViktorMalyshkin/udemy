8ffa84ab491d9603d0e7942a7f6d4ce0ea3d97f1
Viktor <malyshkin.viktor@icloud.com>
2021-04-17 11:16:28 +0300

    s12 p163. Passing Dynamic Parameters to Links

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -8,6 +8,7 @@ import {RecipeModel} from '../../recipe.model'
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: RecipeModel
+  @Input() index: number
 
   constructor() {
   }
