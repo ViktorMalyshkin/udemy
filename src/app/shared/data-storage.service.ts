4787045875aaeae754725a0014a47b4fdc00b41e
Viktor <malyshkin.viktor@icloud.com>
2021-04-23 16:32:41 +0300

    s19 p281. Storing Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,14 +1,23 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {RecipeModel} from "../recipes/recipe.model";
+import {RecipeService} from "../services/recipe.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {
   }
 
-  
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes()
+    return this.http.put('https://udemy-course-recipe-book-4a524-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe(
+      response => {
+        console.log(response)
+      }
+    )
+  }
+
 }
