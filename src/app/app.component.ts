091fd2af4906afc914c52c170829af4db391ade5
Viktor <malyshkin.viktor@icloud.com>
2021-04-27 17:44:48 +0300

    s20 p304. Adding Auto-Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core'
+import {Component, OnInit} from '@angular/core'
+import {AuthService} from "./auth/auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'udemy-course-project'
+
   // loadedFeature = 'recipe'
+
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.authService.autoLogin()
+  }
 
   // onNavigate(feature: string): void {
   //   this.loadedFeature = feature
