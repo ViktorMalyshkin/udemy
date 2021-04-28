080b11df28e262de312145cf302340cce4aba4ea
Viktor <malyshkin.viktor@icloud.com>
2021-04-28 16:03:36 +0300

    s22 p331. Implementing Lazy Loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,8 @@ import {BrowserModule} from '@angular/platform-browser'
 
 import {AppComponent} from './app.component'
 import {HeaderComponent} from './header/header.component'
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {AppRoutingModule} from "./app-routing.module";
-import {RecipeService} from "./recipes/recipe.service";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {AuthComponent} from './auth/auth.component';
-import {AuthInterceptorService} from "./auth/auth-interceptor.service";
-import {AlertComponent} from './shared/alert/alert.component';
-import {RecipesModule} from "./recipes/recipes.module";
+import {HttpClientModule} from "@angular/common/http";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {SharedModule} from "./shared/shared.module";
 import {CoreModule} from "./core.module";
@@ -26,7 +19,6 @@ import {AuthModule} from "./auth/auth.module";
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule,
     ShoppingListModule,
     AuthModule,
     SharedModule,
