647a46b5b963a756df830f865e154d424778e817
Viktor <malyshkin.viktor@icloud.com>
2021-04-28 13:05:08 +0300

    s22 p323. Splitting Modules Correctly

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,12 +1,13 @@
-import  { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {RouterModule} from "@angular/router";
+import {ReactiveFormsModule} from "@angular/forms";
 import {RecipesComponent} from "./recipes.component";
 import {RecipeListComponent} from "./recipe-list/recipe-list.component";
 import {RecipeDetailComponent} from "./recipe-detail/recipe-detail.component";
 import {RecipeItemComponent} from "./recipe-list/recipe-item/recipe-item.component";
 import {RecipeStartComponent} from "./recipe-start/recipe-start.component";
 import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
-
 
 
 @NgModule({
@@ -19,7 +20,9 @@ import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
     RecipeEditComponent,
   ],
   imports: [
-    // CommonModule
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule
   ],
   exports: [
     RecipesComponent,
@@ -30,4 +33,5 @@ import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
     RecipeEditComponent,
   ]
 })
-export class RecipesModule { }
+export class RecipesModule {
+}
