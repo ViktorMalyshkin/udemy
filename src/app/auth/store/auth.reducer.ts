e5f6f1fa9ce07b261479e1eba13bc36c77fb8d94
Viktor <malyshkin.viktor@icloud.com>
2021-04-30 01:26:24 +0300

    s24 p361. Setting Up Auth Reducer & Actions

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,3 +1,4 @@
+import {AuthActions} from "./auth.actions";
 import {User} from "../user.model";
 
 export interface State {
@@ -9,7 +10,26 @@ const initialState: State = {
   user: null
 }
 
-export function authReducer(state = initialState, action) {
-  return state
+export function authReducer(state = initialState, action: AuthActions) {
+  switch (action.type) {
+    case AuthActions.LOGIN:
+      const user = new User(
+        action.payload.email,
+        action.payload.userId,
+        action.payload.token,
+        action.payload.expirationDate
+      )
+      return {
+        ...state,
+        user
+      }
+    case AuthActions.LOGOUT:
+      return {
+        ...state,
+        user: null
+      }
+    default:
+      return state
+  }
 }
 
--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -2,6 +2,7 @@ import {ActionReducerMap} from "@ngrx/store";
 import * as fromShoppingList from "../shopping-list/store/shopping-list.reducer";
 import * as fromAuth from "../auth/store/auth.reducer";
 
+
 export interface AppState {
   shoppingList: fromShoppingList.State
   auth: fromAuth.State
