db9c826e6961aeaf2c1560b136017263eb5e1f96
Viktor <malyshkin.viktor@icloud.com>
2021-04-30 16:21:57 +0300

    s24 p370. Finishing the Login Effect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,10 +1,11 @@
 import {Actions, Effect, ofType} from "@ngrx/effects";
 import * as AuthActions from './auth.actions'
-import {catchError, map, switchMap} from "rxjs/operators";
+import {catchError, map, switchMap, tap} from "rxjs/operators";
 import {environment} from "../../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {of} from "rxjs";
 import {Injectable} from "@angular/core";
+import {Router} from "@angular/router";
 
 export interface AuthResponseData {
   idToken: string;
@@ -17,6 +18,7 @@ export interface AuthResponseData {
 
 @Injectable()
 export class AuthEffects {
+
   @Effect() authLogin = this.actions$.pipe(
     ofType(AuthActions.LOGIN_START),
     switchMap((authData: AuthActions.LoginStart) => {
@@ -31,21 +33,44 @@ export class AuthEffects {
         map(resData => {
           const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
 
-          return of(new AuthActions.Login({
+          return new AuthActions.Login({
             email: resData.email,
             userId: resData.localId,
             token: resData.idToken,
             expirationDate
-          }))
+          })
         }),
-        catchError(error => {
-          return of()
+        catchError(errorRes => {
+          let errorMessage = 'An unknown error occurred!'
+          if (!errorRes.error || !errorRes.error.error) {
+            return of(new AuthActions.LoginFail(errorMessage))
+          }
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'This email exists already!'
+              break
+            case 'EMAIL_NOT_FOUND':
+              errorMessage = 'This email does not exist!';
+              break;
+            case 'INVALID_PASSWORD':
+              errorMessage = 'This password is not correct!';
+              break;
+          }
+          return of(new AuthActions.LoginFail(errorMessage))
         }),
       )
     })
   )
 
-  constructor(private actions$: Actions, private http: HttpClient) {
+  @Effect({dispatch: false})
+  authSuccess = this.actions$.pipe(
+    ofType(AuthActions.LOGIN),
+    tap(() => {
+      this.router.navigate(['/'])
+    })
+  )
+
+  constructor(private actions$: Actions, private http: HttpClient, private router: Router) {
   }
 
 }
